2b43977e3a4d83b48a2616f85950cc362b390827
Martin Boros <mboros04@gmail.com>
2022-12-27 17:04:29 -0500

    new tests

--- test/queue_tests.cpp
+++ test/queue_tests.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <thread>
 #include <atomic>
+#include <string>
 
 TEST(QueueTest, size) {
     LockFreeQueue<int> q;
@@ -56,7 +57,62 @@ TEST(QueueTest, runWithConcurrentConsumersProducers) {
                     }
                 }
             }
-            // Wait until the sentinel count reaches the expected value
+            std::unique_lock<std::mutex> lock(m);
+            cv.wait(lock, [&sentinelCount] { return sentinelCount == THREADS; });
+        });
+    }
+
+    for (auto& t : producers) {
+        t.join();
+    }
+    for (auto& t : consumers) {
+        t.join();
+    }
+
+    for (const auto& n : countEachNumber) {
+        ASSERT_EQ(THREADS, n);
+    }
+}
+
+TEST(QueueTest, queueWithNonTrivialType) {
+    const int THREADS = 20;
+
+    const int NUMS = 100000;
+
+    const std::string SENTINEL = "SENTINEL";
+    LockFreeQueue<std::string> q;
+
+    std::vector<std::atomic<int>> countEachNumber(NUMS);
+
+    std::vector<std::thread> producers;
+
+    for (int i = 0; i < THREADS; ++i) {
+        producers.emplace_back([&q, &SENTINEL] {
+            for (int j = 0; j < NUMS; ++j) {
+                q.push(std::to_string(j));
+            }
+            q.push(SENTINEL);
+        });
+    }
+    std::atomic<int> sentinelCount = 0;
+
+    std::mutex m;
+    std::condition_variable cv;
+
+    std::vector<std::thread> consumers;
+    for (int i = 0; i < THREADS; ++i) {
+        consumers.emplace_back([&q, &sentinelCount, &countEachNumber, &cv, &m, &SENTINEL] {
+            std::string value;
+            while (sentinelCount != THREADS) {
+                if (q.pop(value)) {
+                    if (value == SENTINEL) {
+                        sentinelCount++;
+                        cv.notify_all();
+                    } else {
+                        countEachNumber.at(atoi(value.c_str()))++;
+                    }
+                }
+            }
             std::unique_lock<std::mutex> lock(m);
             cv.wait(lock, [&sentinelCount] { return sentinelCount == THREADS; });
         });
